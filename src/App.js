587f58e17d08103b4912cb8254268b0bb14348ed
ineserdoura <ineserdoura@users.noreply.github.com>
2020-07-15 16:15:09 +0100

    Added GitHub repository link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <footer className="App-footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This page was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/in%C3%AAs-serdoura/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Inês Serdoura
+          </a>{" "}
+          and it's{" "}
+          <a
+            href="https://github.com/ineserdoura/weather-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Built with</p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </footer>
     </div>
   );
 }
