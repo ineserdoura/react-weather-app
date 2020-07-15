d7603942d1e954208cf15b5f91d9ef3fff3c9d28
ineserdoura <ineserdoura@users.noreply.github.com>
2020-07-15 16:28:06 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <footer className="App-footer">
-        <p>
-          This page was coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/in%C3%AAs-serdoura/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Inês Serdoura
-          </a>{" "}
-          and it's{" "}
-          <a
-            href="https://github.com/ineserdoura/weather-react-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-        </p>
-        <p>Built with</p>
-        <img src={logo} className="App-logo" alt="logo" />
-      </footer>
+      <div className="container-fluid">
+        <footer className="App-footer">
+          <p>
+            This page was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/in%C3%AAs-serdoura/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Inês Serdoura
+            </a>{" "}
+            and it's{" "}
+            <a
+              href="https://github.com/ineserdoura/weather-react-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+          </p>
+
+          <p>Built with</p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </footer>
+      </div>
     </div>
   );
 }
